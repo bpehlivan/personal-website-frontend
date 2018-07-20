5f44186c9a4727b36b8a5dcdc0d418ef34badcbe
bpehlivan <bpehlivan235@gmail.com>
2018-07-21 00:52:25 +0300

    Added user registration

--- src/app/beforeLogin/register/register.component.ts
+++ src/app/beforeLogin/register/register.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from '../../user.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  username: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  password2: string;
+  response: string;
+  doesUsernameExists = false;
+  error = 'empty error';
+  isRegistrationFail = false;
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
   }
 
+  register() {
+    this.doesUsernameExists = false;
+    this.userService.userRegistration(
+      this.username, this.firstName, this.lastName,
+      this.email, this.password)
+      .subscribe(
+        resp => {
+          this.response = resp['response'];
+          console.log(resp);
+        },
+        error => {
+          this.isRegistrationFail = true;
+        }
+      );
+    console.log(this.error);
+  }
+
+  clear(): void {
+    this.username = '';
+    this.firstName = '';
+    this.lastName = '';
+    this.email = '';
+    this.password = '';
+    this.password2 = '';
+  }
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -10,14 +10,41 @@ export interface AuthToken {
   username: string;
 }
 
+export interface Register {
+  response: string;
+}
+
 @Injectable()
 export class UserService {
 
   readonly host: string = 'http://127.0.0.1:8000/app';
   token: string;
   constructor(private http: HttpClient) { }
+
+  userRegistration(username: string, firstname: string,
+    lastname: string, email: string, password: string) {
+    const url = '/register';
+    console.log(this.host + url);
+    const requestHeader = new HttpHeaders(
+      {
+        'Content-Type': 'application/json'
+      }
+    );
+    const body = {
+      'username': username,
+      'email': email,
+      'password': password,
+      'first_name': firstname,
+      'last_name': lastname
+    };
+    return this.http.post<Register>(this.host + url, body,
+      {
+        headers: requestHeader
+      });
+  }
+
   userAuthantication(username: string, password: string) {
-    const url = '/login/';
+    const url = '/login';
     console.log(this.host + url);
     const requestHeader = new HttpHeaders({ 'Content-Type': 'application/json' });
     return this.http.post<AuthToken>(this.host + url,
