6adc1547f55ac51c23da8166431772edb056a042
bpehlivan <bpehlivan235@gmail.com>
2018-07-29 17:57:32 +0300

    added 'new record' component and bootstrap modal

--- src/app/afterlogin/afterlogin.module.ts
+++ src/app/afterlogin/afterlogin.module.ts
@@ -5,12 +5,13 @@ import { AfterloginRoutingModule } from './afterlogin-routing.module';
 import { NavbarComponent } from './navbar/navbar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MainChartComponent } from './main-chart/main-chart.component';
+import { NewrecordComponent } from './newrecord/newrecord.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AfterloginRoutingModule
   ],
-  declarations: [NavbarComponent, DashboardComponent, MainChartComponent]
+  declarations: [NavbarComponent, DashboardComponent, MainChartComponent, NewrecordComponent]
 })
 export class AfterloginModule { }
---
+++ src/app/afterlogin/newrecord/newrecord.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newrecord',
+  templateUrl: './newrecord.component.html',
+  styleUrls: ['./newrecord.component.css']
+})
+export class NewrecordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
